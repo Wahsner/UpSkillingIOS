fae0596c4eb16c3a35acb928f5b8a3216d57e734
Wahsner <47725355+Wahsner@users.noreply.github.com>
2024-06-20 21:58:58 +0100

    Completed Section One

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Thomas Renshaw on 13/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
